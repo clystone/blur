b07ba504c4d7093cfd76eeb6cc4a03d550d1d964
clystone <1049267602@qq.com>
2017-07-05 17:23:32 +0800

    PC端完成

--- blur.js
+++ blur.js
@@ -11,14 +11,20 @@ canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
 var image =  new Image();
-var clippingRegion = {x:400,y:200,r:50};
+var radius = 50;
+var clippingRegion = {x:-1,y:-1,r:radius};
 image.src = "image.jpg";
 image.onload = function () {
     initCanvas()
 };
 
 function initCanvas(){
-    draw(image)
+    clippingRegion = {
+        x:Math.random()*(canvasWidth-2*radius)+radius,
+        y:Math.random()*(canvasHeight-2*radius)+radius,
+        r:radius
+    };
+    draw(image,clippingRegion)
 }
 
 function setClippingRegion(clippingRegion){
@@ -27,11 +33,29 @@ function setClippingRegion(clippingRegion){
     context.clip();
 }
 
-function draw(image){
+function draw(image,clippingRegion){
     context.clearRect(0,0,canvas.width,canvas.height);
 
     context.save();
     setClippingRegion(clippingRegion);
     context.drawImage(image,0,0);
     context.restore();
+}
+
+function show(){
+    var timer = setInterval(
+        function () {
+            console.log(111);
+            clippingRegion.r += 20;
+            draw(image,clippingRegion);
+            if (clippingRegion.r > Math.sqrt(canvasHeight * canvasHeight + canvasWidth*canvasWidth)){
+                clearInterval(timer)
+            }
+        },
+        30
+    )
+}
+
+function reset(){
+    initCanvas()
 }
